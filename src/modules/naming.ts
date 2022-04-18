f0b96a3d68ed7c65211d525e1fcb91cde3238834
Chiyuki Kana <chiyukikana@gmail.com>
2022-04-18 22:12:19 +0800

    feat(naming): 完善玩家名称模块

--- src/modules/naming.ts
+++ src/modules/naming.ts
@@ -1,29 +1,57 @@
+import anime from "animejs";
 import localstorage from "./localstorage";
 
 const namingElements = {
   modal: document.querySelector("#name-modal")!,
-  ok: document.querySelector("#name-ok")!,
+  confirm: document.querySelector("#name-confirm")!,
   tip: document.querySelector("#name-tip")!,
   input: <HTMLInputElement>document.querySelector("#name-input"),
+  isNaming: document.querySelector("#is-naming")!,
+  welcomeBar: document.querySelector("#welcome-bar")!,
 };
-const { modal, ok, tip, input } = namingElements;
+const { modal, confirm, tip, input, isNaming, welcomeBar } = namingElements;
+const showWelcomeBar = (name: string) => {
+  isNaming.remove();
+  welcomeBar.querySelector("h1")!.innerHTML = `欢迎回来，${name}！`;
+  welcomeBar.classList.remove("hidden");
+  const timeline = anime.timeline({
+    targets: modal,
+  });
+  timeline.add({
+    duration: 1000,
+    translateY: [600, 0],
+  });
+  timeline.add({
+    opacity: 0,
+    duration: 400,
+    easing: "easeOutSine",
+    complete() {
+      modal.remove();
+    },
+  });
+};
 
 if (!localstorage.get().name) {
-  ok.addEventListener("click", () => {
+  anime({
+    targets: modal,
+    duration: 1000,
+    translateY: [-600, 0],
+  });
+  confirm.addEventListener("click", () => {
     if (input.value === "") {
       tip.innerHTML = "名称不能为空！";
-    } else if (input.value.length > 10) {
+    } else if (input.value.length > 15) {
       tip.innerHTML = "名称的长度不能超过10个字符！";
     } else {
       localstorage.save((data) => {
         data.name = input.value;
       });
-      modal.remove();
+      showWelcomeBar(localstorage.get().name);
     }
   });
   input.addEventListener("input", () => {
     tip.innerHTML = "";
   });
 } else {
-  modal.remove();
+  showWelcomeBar(localstorage.get().name);
 }
