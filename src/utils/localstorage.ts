485839c7a101efebf933b402b64e257c39dfcca1
chiyukikana <chiyukikana@gmail.com>
2022-04-21 12:38:50 +0800

    perf(localstorage): 修改本地存储模块的部分功能

--- src/utils/localstorage.ts
+++ src/utils/localstorage.ts
@@ -1,6 +1,10 @@
 const metadata: Metadata = {
   pid: new Date().getTime(),
   name: null,
+  levels: 1,
+  needExp: 100,
+  historyBreak: 0,
+  version: 1.0,
 };
 const db_key = "th_storage";
 const db_name = "th_db";
@@ -9,10 +13,10 @@ const db_name = "th_db";
  */
 const load = () => {
   if (localStorage.getItem(db_key)) {
-    console.log("Loading...");
+    import.meta.env.DEV && console.log("Loading...");
   } else {
     localStorage.setItem(db_key, JSON.stringify(metadata));
-    console.log("Init...");
+    import.meta.env.DEV && console.log("Init...");
   }
   Object.defineProperty(window, db_name, {
     value: JSON.parse(localStorage.getItem(db_key)!),
@@ -27,21 +31,20 @@ load();
  * @param callback 修改临时进度的回调，可以进行临时进度的修改，回调执行完后会保存当前进度。
  */
 const save = (callback?: (data: Metadata) => void) => {
-  const db_temp = (window as any)[db_name] as typeof metadata;
+  const db_temp = <Metadata>(<any>window)[db_name];
   try {
     callback?.(db_temp);
     localStorage.setItem(db_key, JSON.stringify(db_temp));
-    console.log("Saved!", db_temp);
+    import.meta.env.DEV && console.log("Saved!", db_temp);
   } catch (e) {
     console.error(e);
   }
 };
-
 /**
  * 获取当前临时进度对象
  */
 const get = () => {
-  return (window as any)[db_name] as typeof metadata;
+  return <Metadata>(<any>window)[db_name];
 };
 
 export { db_key, db_name };
@@ -50,4 +53,8 @@ export default { load, save, get };
 interface Metadata {
   pid: number;
   name: any;
+  levels: number;
+  needExp: number;
+  historyBreak: number;
+  version: number;
 }
