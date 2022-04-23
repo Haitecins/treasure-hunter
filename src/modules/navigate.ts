536d6f66892882e848683617419cb0e08edb466e
Chiyuki Kana <chiyukikana@gmail.com>
2022-04-23 15:07:07 +0800

    perf(navigate): 优化实体的运动逻辑

--- src/modules/navigate.ts
+++ src/modules/navigate.ts
@@ -2,70 +2,44 @@ import anime from "animejs";
 import { Entity } from "./entities";
 
 const navigate = (el: Entity) => {
-  const timeline = anime.timeline({
-    targets: el,
-  });
-  const x = Math.floor(
-    Math.random() * (document.documentElement.clientWidth - el.clientWidth)
-  );
-  const y = Math.floor(
-    Math.random() * (document.documentElement.clientHeight - el.clientHeight)
-  );
+  const { clientWidth, clientHeight } = document.documentElement;
+  const timeline = anime.timeline({ targets: el });
 
   // 将路线动画实例对象给tracker属性控制
   el.tracker = timeline;
-  // 实体生成后改变其位置
-  timeline.add({
-    duration: 0,
-    easing: "linear",
-    translateX: [x, x],
-    translateY: [y, y],
+  // 实体生成后改变生成的位置
+  anime.set(el, {
+    translateX: anime.random(0, clientWidth - el.clientWidth),
+    translateY: anime.random(0, clientHeight - el.clientHeight),
   });
-  // 添加生成动画
+  // 实体出现的过渡效果
   timeline.add({
     duration: 1000,
     scale: [0, 1],
     opacity: [0, 1],
   });
-  // 移动动画
   timeline.add({
-    duration: Math.floor(Math.random() * 5000) + 2000,
+    duration: anime.random(2000, 5000),
+    easing: "linear",
     keyframes: [
       {
-        translateX: Math.floor(
-          Math.random() *
-            (document.documentElement.clientWidth - el.clientWidth)
-        ),
-        translateY: Math.floor(
-          Math.random() *
-            (document.documentElement.clientHeight - el.clientHeight)
-        ),
+        translateX: anime.random(0, clientWidth - el.clientWidth),
+        translateY: anime.random(0, clientHeight - el.clientHeight),
       },
       {
-        translateX: Math.floor(
-          Math.random() *
-            (document.documentElement.clientWidth - el.clientWidth)
-        ),
-        translateY: Math.floor(
-          Math.random() *
-            (document.documentElement.clientHeight - el.clientHeight)
-        ),
+        translateX: anime.random(0, clientWidth - el.clientWidth),
+        translateY: anime.random(0, clientHeight - el.clientHeight),
       },
       {
-        translateX: Math.floor(
-          Math.random() *
-            (document.documentElement.clientWidth - el.clientWidth)
-        ),
-        translateY: Math.floor(
-          Math.random() *
-            (document.documentElement.clientHeight - el.clientHeight)
-        ),
+        translateX: anime.random(0, clientWidth - el.clientWidth),
+        translateY: anime.random(0, clientHeight - el.clientHeight),
       },
     ],
   });
   timeline.add({
     delay: 500,
-    duration: 1000,
+    duration: 300,
+    easing: "easeInOutQuad",
     scale: [1, 0],
     opacity: [1, 0],
     complete() {
