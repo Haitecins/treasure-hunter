76aaf418034218b5a47782884a0c91e0c16b244a
Chiyuki Kana <chiyukikana@gmail.com>
2022-04-23 12:39:23 +0800

    perf(auth): 优化代码逻辑

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -11,8 +11,36 @@ const authObject = {
   welcomeBar: document.querySelector("#welcome-bar")!,
 };
 
-// 显示欢迎消息的功能
-const showWelcomeBar = () => {
+// 初始化模块
+const init = () => {
+  const { modal, confirm, tip, input } = authObject;
+  // 命名处理程序
+  const loginHandler = () => {
+    if (input.value === "" || input.value.trim() === "") {
+      tip.innerHTML = "名称不能为空！";
+    } else if (input.value.length > 15) {
+      tip.innerHTML = "名称的长度不能超过10个字符！";
+    } else {
+      localstorage.save((data) => {
+        data.name = input.value.trim();
+      });
+      success();
+    }
+  };
+
+  anime({
+    targets: modal,
+    duration: 1000,
+    translateY: [-600, 0],
+  });
+  confirm.addEventListener("click", loginHandler);
+  input.addEventListener("keydown", (ev) => {
+    tip.innerHTML = "";
+    ev.code === "Enter" && loginHandler();
+  });
+};
+// 模块验证通过
+const success = () => {
   const { modal, isLogin, welcomeBar } = authObject;
   const timeline = anime.timeline({
     targets: modal,
@@ -48,35 +76,10 @@ const showWelcomeBar = () => {
 
 if (!localstorage.get().name) {
   console.log("初始化Auth模块");
-
-  const { modal, confirm, tip, input } = authObject;
-  // 命名处理程序
-  const loginHandler = () => {
-    if (input.value === "" || input.value.trim() === "") {
-      tip.innerHTML = "名称不能为空！";
-    } else if (input.value.length > 15) {
-      tip.innerHTML = "名称的长度不能超过10个字符！";
-    } else {
-      localstorage.save((data) => {
-        data.name = input.value.trim();
-      });
-      showWelcomeBar();
-    }
-  };
-
-  anime({
-    targets: modal,
-    duration: 1000,
-    translateY: [-600, 0],
-  });
-  confirm.addEventListener("click", loginHandler);
-  input.addEventListener("keydown", (ev) => {
-    tip.innerHTML = "";
-    ev.code === "Enter" && loginHandler();
-  });
+  init();
 } else {
   console.log("Auth模块验证通过");
-  showWelcomeBar();
+  success();
 }
 
 export default authObject;
