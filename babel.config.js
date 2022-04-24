5eb9ffa038812622812e4bb4dd3ad30f06089391
BakaKana <chiyukikana@icloud.com>
2022-04-24 19:11:01 +0800

    feat(vanilla): 使用原生JSX

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    "@babel/plugin-transform-react-jsx",
+    {
+      pragma: "h",
+      pragmaFrag: "DocumentFragment",
+    },
+  ],
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     "./index.html",
     "./src/modules/**/*.{js,ts}",
+    "./src/providers/**/*.{jsx,tsx}",
     "./src/utils/**/*.{js,ts}",
   ],
   theme: {
