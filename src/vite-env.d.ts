bc596ba3525f2a49a580a569d72202bf1438abc8
BakaKana <chiyukikana@gmail.com>
2022-05-18 19:58:38 +0800

    fix(types): fix err in deploy.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,15 @@
 /// <reference types="vite/client" />
-/// <reference types="dayjs/plugin/relativeTime" />
+import { Dayjs } from "dayjs";
+
+type DateType = string | number | Date | Dayjs;
+declare module "dayjs" {
+  interface Dayjs {
+    fromNow(withoutSuffix?: boolean): string;
+
+    from(compared: DateType, withoutSuffix?: boolean): string;
+
+    toNow(withoutSuffix?: boolean): string;
+
+    to(compared: DateType, withoutSuffix?: boolean): string;
+  }
+}
