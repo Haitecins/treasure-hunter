8d8a0a0e80964e0974726aa2d252677acd3a3f2e
BakaKana <chiyukikana@gmail.com>
2022-05-23 19:55:32 +0800

    feat(Howl): init

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { Howler } from "howler";
 import * as dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/zh-cn";
 import "./assets/styles/index.css"; // CSS文件
 import "./conf/env"; // 环境功能配置
 import createApp from "./modules/createApp";
+
+// Howler.js 全局配置
+Howler.volume(0.5);
 
 // Day.js 全局配置
 dayjs.locale("zh-cn");
