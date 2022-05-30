cc38dc41f50d4d080af52d26048c745864e2be71
BakaKana <chiyukikana@gmail.com>
2022-05-30 10:59:28 +0800

    perf: move folders

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { Howler } from "howler";
 import * as dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/zh-cn";
-import "./assets/styles/index.css"; // CSS文件
-import "./conf/env"; // 环境功能配置
+import "./styles/index.css";
+import "./conf/env";
 import createApp from "./modules/createApp";
 
 // Howler.js 全局配置
